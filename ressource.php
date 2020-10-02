edf68be68f367d1313741e659ac53a31bfa0a7e4
riogrande75 <riogrande75@users.noreply.github.com>
2020-10-02 11:00:47 +0200

    Update ressource.php

--- ressource.php
+++ ressource.php
@@ -1,7 +1,11 @@
 #!/usr/bin/php
 <?php
 $debug=0;
-
+$logfilename = "/etc/eauto/log/api_"; //Path for logfiles, needs to be created
+if($argc>1) //if called with "info" display debug output
+        {
+        if($argv[1] == "info") $debug=1;
+        }
 $FIN = "WDD242xxx";
 $api_url_soc = "https://api.mercedes-benz.com/vehicledata/v2/vehicles/".$FIN."/resources/soc"; //StateOfCharge only
 $api_url_range = "https://api.mercedes-benz.com/vehicledata/v2/vehicles/".$FIN."/resources/rangeelectric"; //range only
@@ -32,7 +36,23 @@ function getResource($access_token) {
         if($status==204)
                 {
                 if($debug) echo "Received empty response with status code 204. Vehicle did not provide update for >12h. Keeping soc unchanged.\n";
-                exit(204);
+                exit($status);
+                }
+        if($status==402 || $status==403 || $status==404)
+                {
+                if($debug) echo "Received status code $status\n";
+                $exVeErrorId = json_decode($responseS,true)['exVeErrorId'];
+                $exVeErrorMsg = json_decode($responseS,true)['exVeErrorMsg'];
+                if($debug) echo "ErrorID:$exVeErrorId, $exVeErrorMsg\n";
+                logging("ERROR: ErrorID: $exVeErrorId, $exVeErrorMsg");
+                exit($status);
+                }
+        if($status==500 || $status==502 || $status==502 || $status==503)
+                {
+                if($debug) echo "Received status code $status\n";
+                $fault =  json_decode($responseS,true)['fault']['faultstring'];
+                if($debug) echo "Fault: $fault\n";
+                logging("FAULT: $fault");
                 }
         logging("Status $status empfangen. Emfangen wurde $responseS");
         sleep(3);
